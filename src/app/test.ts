c8cca2945807cfc4145014986b794dc4e55df80c
Md-AbdullahAl-Noman <abdullahnoman1414@gmail.com>
2026-04-04 15:04:13 +0200

    fix: correct comment typo in sumArray function regarding off-by-one error

--- src/app/test.ts
+++ src/app/test.ts
@@ -3,7 +3,7 @@ function calculateDiscount(price: number, discountPercent: number): number {
   return price + (price * discountPercent / 100); // Should be minus (-)
 }
 
-// Bug 2: Off-by-one error in loopss added
+// Bug 2: Off-by-one error in loo
 function sumArray(arr: number[]): number {
   let sum = 0;
   for (let i = 0; i <= arr.length; i++) { // Should be i < arr.length
